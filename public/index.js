019e9eee03526e17418c918bb911cde681dac4ab
baramsivaramireddy <baramsivarami@gmail.com>
2022-09-22 21:16:16 +0530

    added sign up page

--- public/index.js
+++ public/index.js
@@ -3,7 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { auth } from "firebase/auth"
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword , 
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification} from "firebase/auth"
 import { firestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,4 +25,91 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-console.log(firebase)
+const auth = getAuth();
+
+
+let user = auth.currentUser;
+
+const createAcount = () => {
+  
+  let email = document.getElementById('email').value;
+  let password  = document.getElementById('password').value;
+  let confromPassword = document.getElementById('comformPassword').value;
+  
+  if (password === confromPassword) { 
+          createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+             user = userCredential.user;
+            console.log(user)
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+                alert(errorMessage)
+          });
+        }
+      else {
+        alert('password mismatch');
+      }
+}
+
+
+const signIn = () => {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    user = userCredential.user;
+   
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+const signout = () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+  
+}
+
+const sendMail = ( ) => {
+  sendEmailVerification(auth.currentUser)
+  .then(() => {
+    // Email verification sent!
+    // ...
+  });
+}
+
+const passwordReset = () =>{
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    // Password reset email sent!
+    // ..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+}
+
+document.getElementById('signIn').addEventListener("click", function(event){
+  event.preventDefault();
+  signIn()
+});
+
+document.addEventListener('DOMContentLoaded' ,() => {
+
+  if (user)  {
+    // ask user to login or show how to sign up 
+  }
+  else {
+
+    // show dash board 
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,11 @@ module.exports = {
   entry: './public/index.js',
   // The location of the build folder described above
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
   devtool: 'eval-source-map',
+  mode: 'development'
 };

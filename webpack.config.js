9afda7840d3e01c78cb21536aecb76e7422fd5e2
baramsivaramireddy <baramsivarami@gmail.com>
2022-09-22 13:08:32 +0530

    added login

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'index.js'
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
